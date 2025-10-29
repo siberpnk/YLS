5a0c767c741057353cd1945611ce1de1b38cae2b
siberpnk <33740115+siberpnk@users.noreply.github.com>
2025-10-30 00:29:35 +1100

    Add project files.

---
+++ YourLocalShopMVC/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YourLocalShopMVC.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        [DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
+        public required decimal Price { set; get; }
+        public int? Stock { set; get; } 
+    }
+}
